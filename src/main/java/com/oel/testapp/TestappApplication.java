58239c61ddf9998ccf261d08bd6c0709f22f927c
oelsons <oelsonseg@hotmail.com>
2021-03-23 22:33:47 -0300

    Minimal app - removed unnecessary files

--- src/main/java/com/oel/testapp/TestappApplication.java
+++ src/main/java/com/oel/testapp/TestappApplication.java
@@ -2,9 +2,6 @@ package com.oel.testapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class TestappApplication {
--- src/main/java/com/oel/testapp/controller/DigimonController.java
+++ src/main/java/com/oel/testapp/controller/DigimonController.java
@@ -1,6 +1,5 @@
 package com.oel.testapp.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
